f056f80df97e085fb99fdc915ee4fc6ac285cd56
Chad Scott <chads@us.ibm.com>
2017-09-27 21:04:46 -0500

    Update getToken() to do more stringent checking of HTTP response

--- misc/http.js
+++ misc/http.js
@@ -1,14 +1,19 @@
 const misc = require('../misc');
+const util = require('util');
 
 function statusCodeOK(res) {
     
-    const whoami = 'watson-workspace.misc.statusCodeOK(): ';
+    const whoami = 'watson-workspace.misc.http.statusCodeOK(): ';
         
     if (res.hasOwnProperty('statusCode') && res.statusCode === 200) {
         misc.debug(whoami + 'Received HTTP status code OK');
         return true;
+    } else if (res.hasOwnProperty('statusCode')) {
+        misc.debug(whoami + 'Received non-OK HTTP status code ' + res.statusCode);
+        return false;
     } else {
-        misc.debug(whoami + 'Received non-OK HTTP status code');
+        misc.debug(whoami + 'Response contains no status code');
+        misc.debug(whoami + util.inspect(res, { depth: null }));
         return false;
     }
     
--- misc/json.js
+++ misc/json.js
@@ -1,12 +1,20 @@
+// Public modules
+const util = require('util');
+
+// Local modules
 const misc = require('../misc');
 
 function assertJSONObject(obj) {
         
-    var jsonError = new Error('Object is not valid JSON');
-    jsonError.name = 'InvalidJSONError';
+    const whoami = 'watson-workspace.misc.json.assertJSONObject(): ';
+    var jsonError;
+    
+    misc.debug(whoami + 'Object:\n' + util.inspect(obj, { depth: null }));
     
     // If obj is not an object, it can't be a JSON object
     if (typeof(obj) !== 'object') {
+        jsonError = new Error("Object is not valid JSON. The typeof is not 'object'");
+        jsonError.name = 'InvalidJSONError';
         throw jsonError;
     }
     
@@ -15,6 +23,8 @@ function assertJSONObject(obj) {
         JSON.parse(JSON.stringify(obj));
     }
     catch (error) {
+        jsonError = new Error('Object is not valid JSON. Unable to convert to string and parse into JSON');
+        jsonError.name = 'InvalidJSONError';
         throw jsonError;
     }
     
