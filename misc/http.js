29c44476fc325d849fbb67cc8be6da90e1045c50
Chad Scott <chads@us.ibm.com>
2017-11-08 14:56:38 -0600

    Various clean-up work in auth and misc

--- misc/http.js
+++ misc/http.js
@@ -1,20 +1,15 @@
+// Local modules
 const misc = require('../misc');
-const util = require('util');
 
-function statusCodeOK(res) {
+/*
+    Throws an error if the status code is not HTTP 200. Otherwise, no-op.
+*/
+function assertStatusCodeOK(res) {
     
-    const whoami = 'watson-workspace.misc.http.statusCodeOK(): ';
-        
-    if (res.hasOwnProperty('statusCode') && res.statusCode === 200) {
-        misc.trace.logTrace(whoami + 'Received HTTP status code OK', misc.trace.TRACE_LEVEL_MEDIUM);
-        return true;
-    } else if (res.hasOwnProperty('statusCode')) {
-        misc.trace.logTrace(whoami + 'Received non-OK HTTP status code ' + res.statusCode, misc.trace.TRACE_LEVEL_MEDIUM);
-        return false;
-    } else {
-        misc.trace.logTrace(whoami + 'Response contains no status code', misc.trace.TRACE_LEVEL_MEDIUM);
-        misc.trace.logTrace(whoami + util.inspect(res, { depth: null }), misc.trace.TRACE_LEVEL_HIGH);
-        return false;
+    if (!res.statusCode) {
+        throw new misc.error('NoStatusCodeError', 'Response contains no status code property');
+    } else if (res.statusCode !== 200) {
+        throw new misc.error('NonOKStatusCodeError', 'Status code is not 200');
     }
     
 }
--- misc/json.js
+++ misc/json.js
@@ -1,6 +1,3 @@
-// Public modules
-const util = require('util');
-
 // Local modules
 const misc = require('../misc');
 
@@ -8,27 +5,18 @@ const misc = require('../misc');
     Throws an error if the object is not JSON. Otherwise, no-op.
 */
 function assertJSONObject(obj) {
-        
-    const whoami = 'watson-workspace.misc.json.assertJSONObject(): ';
-    var jsonError;
-    
-    misc.trace.logTrace(whoami + 'Object:\n' + util.inspect(obj, { depth: null }), misc.trace.TRACE_LEVEL_HIGH);
-    
+                
     // If obj is not an object, it can't be a JSON object
     if (typeof(obj) !== 'object') {
-        jsonError = new Error("Object is not valid JSON. The typeof is not 'object'");
-        jsonError.name = 'InvalidJSONError';
-        throw jsonError;
+        throw new misc.error('InvalidJSONError', "typeof is not 'object'");
     }
     
     // If we can convert to string and parse back into a JSON, it's JSON
     try {
         JSON.parse(JSON.stringify(obj));
     }
-    catch (error) {
-        jsonError = new Error('Object is not valid JSON. Unable to convert to string and parse into JSON');
-        jsonError.name = 'InvalidJSONError';
-        throw jsonError;
+    catch (err) {
+        throw new misc.error('InvalidJSONError', 'Unable to convert to string and parse into JSON');
     }
     
 }
